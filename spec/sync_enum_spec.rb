b53a28ec994fb673f689b3385a315075d9a23b5f
Bruce Tesar <tesar@rutgers.edu>
2021-05-15 17:18:34 -0400

    Initial commit.

---
+++ spec/sync_enum_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SyncEnum do
+  it "has a version number" do
+    expect(SyncEnum::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
